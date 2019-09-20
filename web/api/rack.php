3669acb8a037a902209e91d03ecc438e75600b39
Matthew Zelinsky <zelinsky@udel.edu>
2019-09-20 10:57:06 -0400

    Added some api testing stuff.

---
+++ web/api/rack.php
@@ -0,0 +1,11 @@
+
+<?php
+function generate_rack($n){
+  $tileBag = "AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ";
+  $rack_letters = substr(str_shuffle($tileBag), 0, $n);
+  
+  $temp = str_split($rack_letters);
+  sort($temp);
+  return implode($temp);
+};
+echo generate_rack(7);
